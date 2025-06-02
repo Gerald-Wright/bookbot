7dba174e6c2f036d94455671d8f67a069519cb79
gerald_wright <wrightg@wrightsoftwaresolutions.com>
2025-06-02 12:45:56 -0400

    refactor stats into stat.py file

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from stats import get_num_words
+
 def main():
     text = get_book_text("books/frankenstein.txt")
     if text != None:
-        print(f"{number_of_words(text)} words found in the document")
-
-def number_of_words(content):
-    """
-    return the word count from the contents
-    """
-    return len(content.split())
+        print(f"{get_num_words(text)} words found in the document")
 
 def get_book_text(path_to_file):
     """
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_num_words(content):
+    """
+    return the word count from the contents
+    """
+    return len(content.split())
